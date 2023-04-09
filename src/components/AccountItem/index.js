1f8929a3bca247e5ba3bbf449a17e21eef5673e4
hoanghuybu <dohoanghuy8a3@gmail.com>
2023-04-09 16:20:33 +0700

    Build Header v2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,24 @@
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './AccountItems.module.scss';
+
+function AccountItems() {
+    return (
+        <div className={styles.wrapper}>
+            <img
+                className={styles.avatar}
+                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/b47bf9a7076dade7aa42e8657ef3d3f9~c5_100x100.jpeg?x-expires=1681200000&x-signature=Qmk7icxDOrVONo0RFdhOfeDzwq4%3D"
+                alt="Hoang Huy"
+            ></img>
+            <div className={styles.info}>
+                <h4 className={styles.name}>
+                    <span> Do Hoang Huy </span>
+                    <FontAwesomeIcon className={styles.check} icon={faCheckCircle}></FontAwesomeIcon>
+                </h4>
+                <span className={styles.userName}>huydh</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItems;
--- src/components/Layouts/DefaultLayouts/Header/index.js
+++ src/components/Layouts/DefaultLayouts/Header/index.js
@@ -2,24 +2,53 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItems from '~/components/AccountItem';
 
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 0);
+    });
+
     return (
         <header className={styles.wrapper}>
             <div className={styles.inner}>
                 <div className={styles.logo}>
                     <img src={images.logo} alt="TikTok"></img>
                 </div>
-                <div className={styles.search}>
-                    <input placeholder="Search Account And Video" spellCheck={false}></input>
-                    {/* <button className={styles.clear}>
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={styles.searchResult} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={styles.searchTitle}>Accounts</h4>
+                                <AccountItems />
+                                <AccountItems />
+                                <AccountItems />
+                                <AccountItems />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={styles.search}>
+                        <input placeholder="Search Account And Video" spellCheck={false}></input>
+                        {/* <button className={styles.clear}>
                         <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
                     </button> */}
-                    <FontAwesomeIcon className={styles.loading} icon={faSpinner}></FontAwesomeIcon>
-                    <button className={styles.searchIcon}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
-                    </button>
-                </div>
+                        <FontAwesomeIcon className={styles.loading} icon={faSpinner}></FontAwesomeIcon>
+
+                        <button className={styles.searchIcon}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+                        </button>
+                    </div>
+                </Tippy>
                 <div className={styles.actions}></div>
             </div>
         </header>
